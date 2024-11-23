a64bf586e5f16e9e691ee43a2c187836ed642a65
Aj connect <ajiriiviero2021@gmail.com>
2024-11-23 21:35:18 +0100

    login,logout,forms,category

--- ecommerce/store/views.py
+++ ecommerce/store/views.py
@@ -1,10 +1,78 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import *
+#login and logout
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+#registers
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from django import forms
+from .forms import SignUpForm
 
-# Create your views here.
+
+def category(request,catry):
+    # replace hyphens with Spaces
+    catry = catry.replace('-', ' ')
+    # grab the category from the url
+    try:
+        # look up the category
+        category = Category.objects.get(name=catry)
+        products = Product.objects.filter(category=category)
+        return render(request, 'category.html', {'products':products, 'category':category})
+    except:
+        messages.success(request, ("That Category Doesn't Exist..."))
+        return redirect('home')
+
+
+def product(request,pk):
+    product = Product.objects.get(id=pk)
+    return render(request, 'product.html', {'product':product})
+
+
 def home(request):
     products = Product.objects.all()
     return render(request, 'home.html', {'products':products})
 
 def about(request):
     return render(request, 'about.html', {})
+
+def login_user(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, ("You have been logging"))
+            return redirect('home')
+        else:
+            messages.success(request, ("There was an error! Try again."))
+            return redirect('login')
+    else:
+        return render(request, 'login.html', {})
+
+
+def logout_user(request):
+    logout(request)
+    messages.success(request, ("You have been logged out...Thanks for stopping by.. "))
+    return redirect('home')
+
+
+def register_user(request):
+    form = SignUpForm()
+    if request.method == "POST":
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            # log in user
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request, ("You have been Registered Successful.. "))
+            return redirect('home')
+        else:
+            messages.success(request, ("Whoops! There problem registering your account. Try again!!"))
+            return redirect('register')
+    else:
+        return render(request, 'register.html', {'form':form})
