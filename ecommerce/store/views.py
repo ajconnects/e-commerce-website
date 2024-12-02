8a79671f4400c7bae30b36bbf855c3eac799e566
Aj connect <ajiriiviero2021@gmail.com>
2024-12-02 20:36:06 +0100

    update user and password

--- ecommerce/store/views.py
+++ ecommerce/store/views.py
@@ -7,7 +7,51 @@ from django.contrib import messages
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from django import forms
-from .forms import SignUpForm
+from .forms import SignUpForm, UpdateUserForm, ChangePasswordForm
+
+
+def update_user(request):
+    if request.user.is_authenticated:
+        current_user = User.objects.get(id=request.user.id)
+        user_form = UpdateUserForm(request.POST or None, instance=current_user)
+
+        if user_form.is_valid():
+            user_form.save()
+
+            login(request, current_user)
+            messages.success(request, "User Has Been Updated!")
+            return redirect('home')
+        return render(request, "update_user.html", {'user_form':user_form})
+    else:
+        messages.success(request, "You have to be Logged In to access this Page")
+        return redirect('home')
+
+
+def update_password(request):
+    if request.user.is_authenticated:
+        current_user = request.user
+        # Did they fill out the form
+        if request.method == 'POST':
+            form = ChangePasswordForm(current_user, request.POST)
+            # Is the form valid
+            if form.is_valid():
+                form.save()
+                messages.success(request, 'Your Password has been Updated.')
+                login(request, current_user)
+                return redirect('update_user')
+            else:
+                #it display the error message redirect
+                for error in list(form.errors.values()):
+                    messages.error(request, error)
+                    return redirect('update_password')
+
+        else:
+            form = ChangePasswordForm(current_user)
+            return render(request, 'update_password.html', {'form':form})
+    else:
+        messages.success(request, ("Your must be logged to view password"))
+        return redirect('home')
+
 
 
 def category(request,catry):
