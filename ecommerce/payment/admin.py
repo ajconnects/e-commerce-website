67195dfd5632abef19b11bdf2d0a84b998a4af26
Aj connect <ajiriiviero2021@gmail.com>
2024-12-17 12:33:04 +0100

    payment

--- ecommerce/payment/admin.py
+++ ecommerce/payment/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import ShippingAddress
 
-# Register your models here.
+# Register your models on the admin sections
+admin.site.register(ShippingAddress)
