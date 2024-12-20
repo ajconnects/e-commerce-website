82abb25d071d54e020f5a4139e1b363ef18c6615
Aj connect <ajiriiviero2021@gmail.com>
2024-12-20 17:45:06 +0100

    billing and payment

--- ecommerce/payment/models.py
+++ ecommerce/payment/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from store.models import Product
+from django.db.models.signals import post_save
 
 
 class ShippingAddress(models.Model):
@@ -19,4 +21,41 @@ class ShippingAddress(models.Model):
 
     def __str__(self):
         return f'Shipping Address - {str(self.id)}'
+
+# Create a user shipping address by default when user signs up
+def create_shipping(sender, instance, created, **kwargs):
+    if created:
+        user_shipping = ShippingAddress(user=instance)
+        user_shipping.save()
+
+# Automate the profile thing
+post_save.connect(create_shipping, sender=User)
+
+
+#Create Order Model
+class Order(models.Model):
+    # Foreign Key
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    full_name = models.CharField(max_length=255)
+    email = models.EmailField(max_length=255)
+    shipping_address = models.TextField(max_length=15000)
+    amount_paid = models.DecimalField(max_digits=12, decimal_places=2)
+    date_ordered = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'Order - {str(self.id)}'
+
+
+# Create Order Items Model
+class OrderItem(models.Model):
+    # Foreign Key
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, null=True, blank=True)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, null=True, blank=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+
+    quantity = models.PositiveBigIntegerField(default=1)
+    price = models.DecimalField(max_digits=12, decimal_places=2)
+
+    def __str__(self):
+        return f"Order Item - {str(self.id)}"
 
--- ecommerce/payment/views.py
+++ ecommerce/payment/views.py
@@ -1,7 +1,55 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from cart.cart import Cart
+from payment.forms import ShippingForm, PaymentForm
+from payment.models import ShippingAddress
 
 
 def payment_success(request):
     return render(request, 'payment/payment_success.html', {})
 
 
+def checkout(request):
+     # Get the cart
+    cart = Cart(request)
+    cart_products = cart.get_prods
+    quantities = cart.get_quants
+    totals = cart.cart_total()
+
+    if request.user.is_authenticated:
+        # checkout as logged in user
+        # shipping user
+        shipping_user = ShippingAddress.objects.get(user__id=request.user.id)
+
+        # Get the User's Shipping Form
+        shipping_form = ShippingForm(request.POST or None, instance=shipping_user)
+        return render(request, "payment/checkout.html", {"cart_products":cart_products, "quantities":quantities, "totals":totals, "shipping_form":shipping_form})
+    else:
+        # checkout as guest
+        shipping_form = ShippingForm(request.POST or None,)
+        return render(request, "payment/checkout.html", {"cart_products":cart_products, "quantities":quantities, "totals":totals, "shipping_form":shipping_form})
+
+
+def billing_info(request):
+    if request.POST:
+        # Get the cart
+        cart = Cart(request)
+        cart_products = cart.get_prods
+        quantities = cart.get_quants
+        totals = cart.cart_total()
+
+        # check to see if user is logged in
+        if request.user.is_authenticated:
+            # Get the billing form
+            billing_form = PaymentForm()
+            return render(request, "payment/billing_info.html", {"cart_products":cart_products, "quantities":quantities, "totals":totals, "shipping_info":request.POST, "billing_form":billing_form})
+        else:
+            # Not logged in
+            billing_form = PaymentForm()
+            return render(request, "payment/billing_info.html", {"cart_products":cart_products, "quantities":quantities, "totals":totals, "shipping_info":request.POST, "billing_form":billing_form})
+
+        shipping_form = request.POST
+        return render(request, "payment/billing_info.html", {"cart_products":cart_products, "quantities":quantities, "totals":totals, "shipping_form":shipping_form})
+    else:
+        messages.success(request, "Access Denied")
+        return redirect('home')
