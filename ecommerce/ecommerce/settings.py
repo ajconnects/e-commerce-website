945aaf2f8353dfc295b2ae2435e4af72487c4aed
Aj connect <ajiriiviero2021@gmail.com>
2025-04-05 17:23:44 +0200

    new

--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -154,19 +154,3 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-
-
-# LOGGING = {
-#     'version': 1,
-#     'handlers': {
-#         'console': {
-#             'class': 'logging.StreamHandler',
-#         },
-#     },
-#     'loggers': {
-#         'django': {
-#             'handlers': ['console'],
-#             'level': 'DEBUG',
-#         },
-#     },
-# }
